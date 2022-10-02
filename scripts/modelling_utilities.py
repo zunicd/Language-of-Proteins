a8cf056c327f95f11b8fd0a995aeeb5f4ffa74b1
Damir Zunic <zunicd@yahoo.com>
2022-10-02 18:13:37 -0400

    wrote comments in modelling_utilities.py and completed running times csv files

--- scripts/modelling_utilities.py
+++ scripts/modelling_utilities.py
@@ -1,28 +1,45 @@
-# Collection of modelling utilities
+# Collection of modelling utilities:
+#    get_emb_folders - prepare paths to embedding folders and fasta files for the given task
+#    fit_tune_CV - tune hyperparameters using GridSearchCV, fit and save models
+#    evaluation - evaluate models and store results into a dataframe
+
+# Import dependencies
 import os
 import pandas as pd
 import joblib
-
 from sklearn.model_selection import GridSearchCV, StratifiedKFold
-
-# Classification metrics
 from sklearn.metrics import accuracy_score, f1_score
 random_state = 10
 
-
-# Function to get embedding folders and fasta files for the task
+# Function to prepare paths to embedding folders and fasta files for the given task
 def get_emb_folders(task, data_folder='../../data'):
+    """ Prepare paths for embedding folders and fasta files
 
+    Args:
+        task: protein groups - ['acp', 'amp', 'dna_binding']
+        data_folder: root of data folder - ['../../data'] default
+
+    Returns:
+        paths: paths to embedding folders; for 'acp,', 'dna_binding'
+            or
+        paths_all: paths to embedding folders; for 'amp'
+        fastas: paths to fasta files;  for 'acp,', 'dna_binding'
+            or
+        all_fa: paths to fasta files; for 'amp'
+    """
     # Defune base folder
     base = os.path.join(data_folder, task)
-    
+    # Initialize lists
     paths_test, test_fa = [], []
     paths_train, train_fa = [], []
     paths_all, all_fa = [], []
+    
+    # Prepare embedding folders
     for root, dirs, files in os.walk(base, topdown=False):
-        #print(root)
+        # Skip root folders with a tail in the list below
         tail = os.path.split(root)[1]
         if tail not in ['esm', 'prose', 'test', 'train', 'all_data']:
+            # Separate folders with 'test', 'train' or 'all' in root
             if 'test' in root:
                 paths_test.append(root)
             elif 'train' in root:
@@ -30,9 +47,13 @@ def get_emb_folders(task, data_folder='../../data'):
             elif 'all' in root:
                 paths_all.append(root)
     
-    # Last folder in a loop is the "base"
+    # Prepare fasta files
+    # The last root folder in the above loop is the "base"
+    # Iterate through files in the "base" and list fasta files paths
     for file in files:
+        # Look for fasta files (.fa extension)
         if 'fa' in file:
+            # Separate files with 'test', 'train' or 'all' in file path
             if 'test' in file:
                 test_fa.append(os.path.join(base, file))
             elif 'train' in file:
@@ -40,25 +61,41 @@ def get_emb_folders(task, data_folder='../../data'):
             else:
                 all_fa.append(os.path.join(base, file))
             
-    
+    # For 'amp' task return paths_all and all_fa
     if len(paths_all) != 0:
         return paths_all, all_fa
     else:
+        # return lists of tuples of (train, test) paths
         fastas = list(zip(train_fa, test_fa))
         paths = list(zip(paths_train, paths_test))
         return paths, fastas
     
 # ===============
     
-# Fit, hypertune and save models
+# Function to tune hyperparameters using GridSearchCV, fit and save models
 def fit_tune_CV(pipe, hyper_params, scorer, path_pt, X_train, y_train, task):
-        
+    """ Tune hyperparameters using GridSearchCV, fit and save models
+
+    Args:
+        pipe: dictionary of models pipelines
+        hyper_params: dictionary of hyperparameters grids
+        scorer: metrics to evaluate the performance of the cross-validated model 
+        path_pt: path to embeding folders, used to create names of the models
+        X_train: features - train embedding vectors
+        y_train: train target variables
+        task: protein groups - ['acp', 'amp', 'dna_binding'],
+              used to name folders for saving models
+
+    Returns:
+        fit_models: dictionary of fitted models 
+    """ 
+    # Made folds by preserving the percentage of samples for each class
     skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=random_state)
 
-    # Create empty dictionary called fitted_models
+    # Create empty dictionary called fit_models
     fit_models = {}
    
-    # Loop through model pipelines, tuning each one and saving it to fitted_models
+    # Loop through model pipelines, tuning each one and saving it to fit_models
     for name, pipeline in pipe.items():
         # Create cross-validation object from pipeline and hyperparameters
         grid = GridSearchCV(pipeline, hyper_params[name], cv=skf,
@@ -84,9 +121,20 @@ def fit_tune_CV(pipe, hyper_params, scorer, path_pt, X_train, y_train, task):
 
 # =============
 
-# Evaluate models
+# Function to evaluate models and store results into a dataframe
 def evaluation(fit_models, X_test, y_test):
+        """ Evaluate models and store results into a dataframe
+
+    Args:
+        fit_models: dictionary of fitted models
+        X_test: test embedding vectors
+        y_train: test target variables
+
+    Returns:
+        eval_df: dictionary of evaluation dataframes
+    """ 
     lst = []
+    # Iterate through models in 'fit_models'
     for name, model in fit_models.items():
         # Get predictions
         y_pred = model.predict(X_test)
